a661be62f33037877fe53beda7ae32a4caaad123
Max Boyar <max.boyar.a@gmail.com>
2020-08-28 15:55:54 +0300

    Delete ref structs, try fix memory leak

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"crypto/tls"
 	"fmt"
 	"github.com/imroc/req"
 	"github.com/meklis/all-ok-radius-server/api/cache"
@@ -9,6 +10,8 @@ import (
 	"github.com/meklis/all-ok-radius-server/prom"
 	"github.com/meklis/all-ok-radius-server/radius/events"
 	"github.com/ztrue/tracerr"
+	"net/http"
+	"net/http/cookiejar"
 	"sync"
 	"time"
 )
@@ -22,6 +25,13 @@ type Api struct {
 }
 
 func Init(conf ApiConfig, lg *logger.Logger) *Api {
+	req.Client().Jar, _ = cookiejar.New(nil)
+	trans, _ := req.Client().Transport.(*http.Transport)
+	trans.MaxIdleConns = 20
+	trans.TLSHandshakeTimeout = 10 * time.Second
+	trans.DisableKeepAlives = true
+	trans.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
 	api := new(Api)
 	api.Conf = conf
 	api.cache = cache.Init(conf.Auth.Caching.TimeoutExpires)
@@ -30,9 +40,9 @@ func Init(conf ApiConfig, lg *logger.Logger) *Api {
 	return api
 }
 
-func (a *Api) Get(req *events.AuthRequest) (*events.AuthResponse, error) {
+func (a *Api) Get(req events.AuthRequest) (events.AuthResponse, error) {
 	hash := req.GetHash()
-	response := new(events.AuthResponse)
+	response := events.AuthResponse{}
 	response, exist := a.cache.Get(hash)
 	if exist {
 		a.lg.DebugF("%v found in cache, check actual time", hash)
@@ -51,7 +61,7 @@ func (a *Api) Get(req *events.AuthRequest) (*events.AuthResponse, error) {
 		a.lg.ErrorF("error get data from api: %v", tracerr.Sprint(err))
 		return response, nil
 	} else if err != nil {
-		return nil, tracerr.Wrap(err)
+		return events.AuthResponse{}, tracerr.Wrap(err)
 	}
 	actualizeTime := time.Now().Add(a.Conf.Auth.Caching.ActualizeTimeout)
 	if actualizeTime.After(time.Now().Add(time.Second * time.Duration(apiResp.LeaseTimeSec))) {
@@ -59,7 +69,7 @@ func (a *Api) Get(req *events.AuthRequest) (*events.AuthResponse, error) {
 		actualizeTime = time.Now().Add(time.Second * time.Duration(apiResp.LeaseTimeSec))
 	}
 	apiResp.Time = actualizeTime
-	a.cache.Set(hash, *apiResp)
+	a.cache.Set(hash, apiResp)
 	return apiResp, nil
 }
 func (a *Api) SendPostAuth(auth PostAuth) {
@@ -104,11 +114,11 @@ func (a *Api) SendAcct(acct events.AcctRequest) {
 	}()
 }
 
-func (a *Api) _getFromApi(request *events.AuthRequest) (*events.AuthResponse, error) {
+func (a *Api) _getFromApi(request events.AuthRequest) (events.AuthResponse, error) {
 	source, err := a.sources.GetSource()
 	if err != nil {
 		a.lg.DebugF("not found sources - %v", err.Error())
-		return nil, tracerr.Wrap(err)
+		return events.AuthResponse{}, tracerr.Wrap(err)
 	}
 	a.lg.DebugF("defined source from rr = %v", source)
 	a.sources.IncRequests(source.Address)
@@ -118,22 +128,22 @@ func (a *Api) _getFromApi(request *events.AuthRequest) (*events.AuthResponse, er
 		prom.ErrorsInc(prom.Error, "api")
 		a.lg.ErrorF("source returned err: %v", tracerr.Sprint(err))
 		a.sources.Disable(source.Address)
-		return nil, tracerr.Wrap(err)
+		return events.AuthResponse{}, tracerr.Wrap(err)
 	}
 	if response.Response().StatusCode != 200 {
 		prom.ErrorsInc(prom.Error, "api")
 		a.lg.ErrorF("source returned http != 200: %v %v", response.Response().StatusCode, response.Response().Status)
 		a.sources.Disable(source.Address)
-		return nil, tracerr.New(fmt.Sprintf("http err: %v - %v", response.Response().StatusCode, response.Response().Status))
+		return events.AuthResponse{}, tracerr.New(fmt.Sprintf("http err: %v - %v", response.Response().StatusCode, response.Response().Status))
 	}
 	apiResp := ApiResponse{}
 	if err := response.ToJSON(&apiResp); err != nil {
 		a.sources.Disable(source.Address)
-		return nil, tracerr.Wrap(err)
+		return events.AuthResponse{}, tracerr.Wrap(err)
 	}
 
 	if apiResp.StatusCode != 200 {
-		return nil, tracerr.New(fmt.Sprintf("api returned status code - %v. must be 200", apiResp.StatusCode))
+		return events.AuthResponse{}, tracerr.New(fmt.Sprintf("api returned status code - %v. must be 200", apiResp.StatusCode))
 	}
-	return &apiResp.Data, nil
+	return apiResp.Data, nil
 }
